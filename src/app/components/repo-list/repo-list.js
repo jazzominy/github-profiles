0f9ab6ae1ad27b9b9d2d209d26019c509181ee95
Jazz <jasmine758@gmail.com>
2018-06-02 23:41:20 +0530

    Added repo stats

--- src/app/components/repo-list/repo-list.js
+++ src/app/components/repo-list/repo-list.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 import "./repo-list.css";
 
 class RepoList extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {};
   }
 
   reposToLi(repos) {
@@ -17,19 +17,43 @@ class RepoList extends Component {
               {r.full_name}
             </a>
           </h3>
-          <span>{r.description}</span>
+          <span id="desc">{r.description}</span>
+          <div id="stats">
+            <div>
+              <span>
+                <svg
+                  className="star"
+                  viewBox="0 0 14 16"
+                  version="1.1"
+                  width="14"
+                  height="16"
+                  aria-hidden="true"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"
+                  />
+                </svg>
+              </span>
+              <span>{r.stargazers_count}</span>
+            </div>
+            <div>
+              <span className="fork" />
+              <span>{r.forks_count}</span>
+            </div>
+          </div>
         </li>
       );
     });
   }
 
-  render() { 
-    return ( 
+  render() {
+    return (
       <div className="repo-grid-wrapper">
         <ul id="repo-grid">{this.reposToLi(this.props.repos)}</ul>
       </div>
     );
   }
 }
- 
+
 export default RepoList;
